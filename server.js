b061285e8f20474541ef1867d78d3f7fc5027877
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-08 19:39:04 -0400

    Expiry Cookie Working

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess ={
   secret: 'Super secret secret',
-  resave: true,
+  cookie: {
+    maxAge: 3600,
+  },
+  resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
